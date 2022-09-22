915cc297fcf405efd31cbfc6ff74d7ed2a4c8c99
Daniel <yedaniel99@gmail.com>
2022-09-22 14:53:11 -0400

    changing links and editing author image

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -1,19 +1,19 @@
 <template>
 	<div class="author">
 
-		<g-image alt="Author image" class="author__image" src="~/assets/images/author.jpg" width="180" height="180" blur="5" />
+		<g-image alt="Author image" class="author__image" src="~/assets/images/author.png" width="180" height="180" blur="5" />
 
 		<h1 v-if="showTitle" class="author__site-title">
 			{{ $static.metadata.siteName }}
 		</h1>
 
 		<p class="author__intro">
-			A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content.
+			Welcome to my portfolio!
 		</p>
 
 		<p class="author__links">
-			<a href="//twitter.com/gridsome">Follow on Twitter</a>
-			<a href="//github.com/gridsome/gridsome-starter-blog">GitHub</a>
+			<a href="//linkedin.com/in/daniel-ye-3a889a243/">LinkedIn</a>
+			<a href="//github.com/danielyedaniel">GitHub</a>
 		</p>
 
 	</div>
